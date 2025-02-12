62be0e50323ef366b4791ea487f7f630cbe44379
YoungBat <165543153+YBTsa@users.noreply.github.com>
2025-02-12 10:08:21 +0800

    Add files via upload

--- docs/js/load.js
+++ docs/js/load.js
@@ -1,12 +1,25 @@
 function load() {
-    let loaders = document.getElementsByClassName("loading");
-    for (let loader of loaders) {
-        loader.hidden = true;
-    }
-    let mains = document.getElementsByClassName("main");
-    for (let main of mains) {
-        main.hidden = false;
-    }
+    addScript('/js/jquery-3.7.1.min.js',function() {
+        $('#head').load('/static/head.html');
+        $('#footer').load('/static/footer.html');
+    });
+    setTimeout(function (){
+        let loaders = document.getElementsByClassName("loading");
+        for (let loader of loaders) {
+            loader.hidden = true;
+        }
+        let mains = document.getElementsByClassName("main");
+        for (let main of mains) {
+            main.hidden = false;
+        }
+    }, 5000);
     console.log("loaded");
 }
-setTimeout(load, 5000);
+function addScript(url,callback) {
+    const script = document.createElement('script');
+    script.setAttribute('type','text/javascript');
+    script.setAttribute('src',url);
+    script.onload = callback;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+load();
